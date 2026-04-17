cb1339fb811e4ff12e760cbb9daeba05d950460d
jtrznadel <trznadeljakub3ai@gmail.com>
2026-04-17 20:09:50 +0200

    initial spring boot project setup

---
+++ src/test/java/com/roam/api/RoamApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roam.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoamApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
